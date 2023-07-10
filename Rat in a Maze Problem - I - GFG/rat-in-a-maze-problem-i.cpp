c14b5c4a119e61296d17bcb91b9548fc937b37dc
EKTA GOEL <71930603+ektagoel-12@users.noreply.github.com>
2023-07-10 13:35:18 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -10,47 +10,34 @@ using namespace std;
 
 class Solution{
     public:
-    void recurPath(int i,int j,vector<vector<int>>& a,int n,vector<string>&ans,string ds,vector<vector<int>>&vis)
+    void recurPath(int i,int j,vector<vector<int>>& a,int n,vector<string>&ans,string ds,
+    vector<vector<int>>&vis,int di[],int dj[])
     {
         if (i == n - 1 && j == n - 1) {
       ans.push_back(ds);
       return;
     }
-        // downward
-    if (i + 1 < n && !vis[i + 1][j] && a[i + 1][j] == 1) {
-      vis[i][j] = 1;
-      recurPath(i + 1, j, a, n, ans, ds + 'D', vis);
-      vis[i][j] = 0;
-    }
-
-    // left
-    if (j - 1 >= 0 && !vis[i][j - 1] && a[i][j - 1] == 1) {
-      vis[i][j] = 1;
-      recurPath(i, j - 1, a, n, ans, ds + 'L', vis);
-      vis[i][j] = 0;
-    }
-
-    // right 
-    if (j + 1 < n && !vis[i][j + 1] && a[i][j + 1] == 1) {
-      vis[i][j] = 1;
-      recurPath(i, j + 1, a, n, ans, ds + 'R', vis);
-      vis[i][j] = 0;
-    }
-
-    // upward
-    if (i - 1 >= 0 && !vis[i - 1][j] && a[i - 1][j] == 1) {
-      vis[i][j] = 1;
-      recurPath(i - 1, j, a, n, ans, ds + 'U', vis);
-      vis[i][j] = 0;
-    }
-
-        
+    string str="DLRU";
+        for(int p=0;p<4;p++)
+        {
+            int nexti=i+di[p];
+            int nextj=j+dj[p];
+            if(nexti>=0&& nextj>=0&& nexti<n && nextj<n && !vis[nexti][nextj] && a[nexti][nextj]==1)
+            {
+                vis[i][j]=1;
+            recurPath(nexti, nextj, a,n,ans,ds+str[p],vis,di,dj);
+            vis[i][j]=0;
+            }
+            
+        }
     }
     vector<string> findPath(vector<vector<int>> &m, int n) {
         // Your code goes here
         vector<string>ans;
         vector<vector<int>>vis(n,vector<int>(n,0));
-        if(m[0][0]==1) recurPath(0,0,m,n,ans,"",vis);
+        int di[]={1,0,0,-1};
+        int dj[]={0,-1,1,0};
+        if(m[0][0]==1) recurPath(0,0,m,n,ans,"",vis,di,dj);
         return ans;
         
     }
