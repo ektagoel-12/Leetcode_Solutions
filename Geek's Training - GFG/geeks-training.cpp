60b16437b83070e808e5fe20272d2d8077d9f5bd
EKTA GOEL <71930603+ektagoel-12@users.noreply.github.com>
2023-08-13 21:58:05 +0530

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.cpp
+++ Geek's Training - GFG/geeks-training.cpp
@@ -5,37 +5,29 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int recur(vector<vector<int>>& points, int n,int last,vector<vector<int>>&dp)
-    {
-        int maxi=0;
-        if(n==0) 
-        {
-            for(int i=0;i<3;i++)
-            {
-                if(i!=last)
-                {
-                    maxi=max(maxi,points[0][i]);
-                }
-            }
-            return dp[n][last]=maxi;
-        }
-        if(dp[n][last]!=-1) return dp[n][last];
-        int point=0;
-        for(int i=0;i<3;i++)
-        {
-            if(i!=last)
-            {
-                point=points[n][i]+recur(points,n-1,i,dp);
-            }
-            maxi=max(maxi,point);
-        }
-        return dp[n][last]=maxi;
-    }
     int maximumPoints(vector<vector<int>>& points, int n) {
-        // Code here
         vector<vector<int>>dp(n,vector<int>(4,-1));
-        return recur(points,n-1,3,dp);
-        
+        dp[0][0]=max(points[0][1],points[0][2]);
+        dp[0][1]=max(points[0][0],points[0][2]);
+        dp[0][2]=max(points[0][0],points[0][1]);
+        dp[0][3]=max(points[0][1],max(points[0][2],dp[0][0]));
+       for(int day=1;day<n;day++)
+       {
+           for(int last=0;last<4;last++)
+           {
+                dp[day][last]=0;
+                for(int task=0;task<3;task++)
+                {
+                    if(task!=last)
+                    {
+                        int point=points[day][task]+dp[day-1][task];
+                        dp[day][last]=max(dp[day][last],point);
+                    }
+                    
+                }
+           }
+       }
+        return dp[n-1][3];
     }
 };
 
