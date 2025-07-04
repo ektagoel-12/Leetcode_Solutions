d409bdc9468307abbb09b60ed776c2c47587775c
EKTA GOEL <71930603+ektagoel-12@users.noreply.github.com>
2025-07-04 09:32:34 +0530

    Time: 11 ms (92.29%), Space: 24.9 MB (76.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,33 +1,15 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t){
-        if(s.size()!=t.size()) return false;
-        int arr[26]={};
-        for(int i=0;i<s.size();i++){
-            arr[s[i]-'a']++;
-            arr[t[i]-'a']--;
-        }
-        for(int i=0;i<26;i++)
-        {
-            if(arr[i]) return false;
-        }
-        return true;
-    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
         vector<vector<string>>ans;
-        vector<bool>flag(strs.size(),false);
-        for(int i=0;i<strs.size();i++){
-            if(flag[i]!=true){
-                vector<string>ds;
-                ds.push_back(strs[i]);
-                for(int j=i+1;j<strs.size();j++){
-                    if(isAnagram(strs[i],strs[j])){
-                        ds.push_back(strs[j]);
-                        flag[j]=true;
-                    }
-                }
-                ans.push_back(ds);
-            }
+        for(auto &itr:strs){
+            string temp=itr;
+            sort(temp.begin(),temp.end());
+            mp[temp].push_back(itr);
+        }
+        for(auto &x:mp){
+            ans.push_back(x.second);
         }
         return ans;
     }
