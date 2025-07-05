7c2a6efe313914466c5893e1f3821bea09e00891
EKTA GOEL <71930603+ektagoel-12@users.noreply.github.com>
2025-07-05 12:02:11 +0530

    Time: 0 ms (100%), Space: 60 MB (56.69%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -4,12 +4,13 @@ public:
        int i=0,j=0,n=nums.size(),ans=0,cnt=0;
        for(j=0;j<n;j++){
         if(nums[j]==0) cnt+=1;
-        if(cnt>1) {
+        while(cnt>1) {
             if(nums[i++]==0){
                 cnt--;
             }
         }
+        ans=max(ans,j-i);
     }
-    return j-i-1;
+    return ans;
     }
 };
